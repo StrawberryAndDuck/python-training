545cb3b84626d9f7d68e1d38d65644cd5dad19f1
suxi519 <gksthgml75@naver.com>
2025-01-09 18:03:54 +0900

    u: crud sql query 수정

--- queries/sample.sql
+++ queries/sample.sql
@@ -6,16 +6,26 @@ CREATE TABLE table_name(
 ) COMMENT '';
 
 
+
 INSERT INTO table_name (id, create_time, name) VALUES (1, "2024-09-09", "sung")
 
 INSERT INTO table_name (id, create_time, name) VALUES (2, "2024-09-10", "suxi")
 
 INSERT INTO table_name (id, create_time, name) VALUES (3, "2024-09-11", "suxi2")
 
-INSERT INTO table_name (id, create_time, name) VALUES (4, "2024-09-12", "suxi3")
+INSERT INTO table_name (id, create_time, name) VALUES (10, "2024-09-12", "suxi3")
 
 INSERT INTO table_name (id, create_time, name) VALUES (5, "2024-09-13", "suxi4")
 
 SELECT * FROM table_name
 
-SELECT * FROM table_name WHERE create_time BETWEEN '2024-09-09' and '2024-09-11' LIMIT 100
+SELECT * FROM table_name WHERE create_time BETWEEN '2024-09-09' and '2024-09-12' LIMIT 100
+
+DESCRIBE table_name
+
+ALTER TABLE table_name COMMENT 'hello world';
+
+SHOW TABLE STATUS WHERE Name = 'table_name';
+
+SELECT DATABASE();
+
